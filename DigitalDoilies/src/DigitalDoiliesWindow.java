124286628654401be0956291e044d9947d8ec501
OllieMartin <olivermartin410@yahoo.co.uk>
2017-03-08 00:13:23 +0000

    Altered size

--- DigitalDoilies/src/DigitalDoiliesWindow.java
+++ DigitalDoilies/src/DigitalDoiliesWindow.java
@@ -13,7 +13,7 @@ public class DigitalDoiliesWindow extends JFrame {
 	
 	public DigitalDoiliesWindow() {
 		super("Digital Doilies");
-		mainPane = new DigitalDoiliesPanel(250);
+		mainPane = new DigitalDoiliesPanel(200);
 		this.add(mainPane);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setSize(500, 500);
