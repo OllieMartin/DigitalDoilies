1d72d92d1ec292f8870919fb938a7f47010155e9
ojm1g16 <ojm1g16@JFB25-R1007-W12.soton.ac.uk>
2017-03-09 14:19:24 +0000

    Gallery works correctly

--- DigitalDoilies/src/GalleryPanel.java
+++ DigitalDoilies/src/GalleryPanel.java
@@ -27,46 +27,56 @@ import javax.swing.border.LineBorder;
 public class GalleryPanel extends JPanel {
 
 	private int width;
+	private int adjustedWidth;
 	private List<JToggleButton> images;
 	//private ButtonGroup buttons;
 
 	public GalleryPanel(int width) {
 		this.width = width;
+		adjustedWidth = width - 20;
 		this.images = new ArrayList<JToggleButton>();
 		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 		//this.buttons = new ButtonGroup();
 	}
 
 	public int getWidth() {
-		return this.width - 10;
+		return this.width;
 	}
 
 	public void addImage(BufferedImage image) {
 		if (images.size() < 12) {
-		JToggleButton newSave = new JToggleButton();
-		Border line = new LineBorder(Color.BLACK);
-		Border margin = new EmptyBorder(1, 0, 1, 0);
-		Border compound = new CompoundBorder(line, margin);
-		newSave.setBorder(compound);
-		float iwidth = image.getWidth();
-		float iheight = image.getHeight();
-		BufferedImage newImage = new BufferedImage(width,(int) (width*(iheight/iwidth)),BufferedImage.TYPE_INT_RGB);
-		System.out.println(width*(iheight/iwidth));
-		newSave.setIcon(new ImageIcon(image.getScaledInstance(width,(int) (width*(iheight/iwidth)),BufferedImage.TYPE_INT_RGB)));
-		newSave.setHorizontalAlignment(SwingConstants.CENTER);
-		newSave.setVerticalAlignment(SwingConstants.CENTER);
-		Graphics2D g2 = (Graphics2D) newImage.getGraphics();
-		g2.drawImage(image.getScaledInstance(width,(int) (width*(iheight/iwidth)),Image.SCALE_DEFAULT),0,0,newSave);
-		g2.setColor(Color.RED);
-		g2.setStroke(new BasicStroke(5,BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
-		g2.drawLine(0, 0, width, (int) (width*(iheight/iwidth)));
-		newSave.setSelectedIcon(new ImageIcon(newImage));
-		newSave.setMinimumSize(new Dimension(width,width));
-		this.add(newSave);
-		images.add(newSave);
-		//buttons.add(newSave);
-		//this.add(Box.createRigidArea(new Dimension(0, 10)));
-		this.updateUI();
+			
+			Image scaledImage;
+			
+			JToggleButton newSave = new JToggleButton();
+			Border line = new LineBorder(Color.BLACK);
+			Border margin = new EmptyBorder(1, 0, 1, 0);
+			Border compound = new CompoundBorder(line, margin);
+			newSave.setBorder(compound);
+			
+			float iwidth = image.getWidth();
+			float iheight = image.getHeight();
+			
+			scaledImage = image.getScaledInstance(adjustedWidth,(int) (adjustedWidth*(iheight/iwidth)),BufferedImage.TYPE_INT_RGB);
+			
+			newSave.setIcon(new ImageIcon(scaledImage));
+			newSave.setHorizontalAlignment(SwingConstants.CENTER);
+			newSave.setVerticalAlignment(SwingConstants.CENTER);
+			
+			BufferedImage selectedImage = new BufferedImage(adjustedWidth,(int) (adjustedWidth*(iheight/iwidth)),BufferedImage.TYPE_INT_RGB);
+			Graphics2D g2 = (Graphics2D) selectedImage.getGraphics();
+			g2.drawImage(scaledImage,0,0, this);
+			g2.setColor(Color.RED);
+			g2.setStroke(new BasicStroke(5,BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+			g2.drawLine(0, 0, adjustedWidth, (int) (adjustedWidth*(iheight/iwidth)));
+			g2.drawLine(adjustedWidth, 0, 0, (int) (adjustedWidth*(iheight/iwidth)));
+			
+			newSave.setSelectedIcon(new ImageIcon(selectedImage));
+			
+			this.add(newSave);
+			images.add(newSave);
+
+			this.updateUI();
 		}
 	}
 
